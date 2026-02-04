98e097a72e641d680b5590fe2df96b494cd869be
JONATHAN RICARDO RUELAS RAMIREZ <jonathan.ruelas@DIRISLC.GOB.PE>
2026-02-03 23:49:54 -0500

    tercer commit

--- main/settings.py
+++ main/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'diris_mesa_de_ayuda',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Administrador@123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
